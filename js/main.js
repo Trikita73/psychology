17edac757fa7d045b7661b2013d675d402d326b9
Trikita73 <diachenkoandrii7@gmail.com>
2022-10-07 15:34:43 +0300

    ps_2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
-$('.container').fsScroll({
+// $('.container').fsScroll({
 
-  beforeScroll: function(el, index) {
-    el.find('h1').addClass('text-animate');
-  },
+//   // beforeScroll: function(el, index) {
+//   //   el.find('h1').addClass('text-animate');
+//   // },
 
-  afterScroll: function(el, index) {
-    el.find('h1').removeClass('text-animate');
-  }
-})
+//   // afterScroll: function(el, index) {
+//   //   el.find('h1').removeClass('text-animate');
+//   // }
+// })
+
+
+//JQuery: He is responsible for the slow scroll and loader -->
+
+$(window).load(function() {
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
